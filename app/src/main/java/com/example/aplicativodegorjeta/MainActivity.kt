86614df0d3cd1dd452a33f4eba38e3c84183714f
Henrique <rick-silva8@outlook.com>
2024-12-10 19:18:42 +0100

    inclusao do radial group e button + recuperacao de dados

--- app/src/main/java/com/example/aplicativodegorjeta/MainActivity.kt
+++ app/src/main/java/com/example/aplicativodegorjeta/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.aplicativodegorjeta
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val btnClean: Button = findViewById(R.id.btn_clean)
+        val btnDone: Button = findViewById(R.id.btn_done)
+        val edtTotal: TextInputEditText = findViewById(R.id.tie_number_total)
+        val edtPeople: TextInputEditText = findViewById(R.id.tie_number_people)
+
+        btnDone.setOnClickListener {
+            println("Henrique" + edtTotal.text)
+            println("Henrique" + edtPeople.text)
+        }
     }
 }
